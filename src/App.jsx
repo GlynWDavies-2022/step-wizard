d6b055d18feb1aaf3864e65df5cc232968e55e98
Glyn Davies <glynwdavies@gmail.com>
2026-08-17 08:00:39 +0100

    Added main layout with styling and logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import './App.css';
 
+const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
+
 function App() {
-    return <></>;
+    const step = 3;
+    function handlePrevious() {}
+    function handleNext() {}
+    return (
+        <div className='steps'>
+            <div className='numbers'>
+                <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+                <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+                <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+            </div>
+            <p className='message'>
+                Step {step}: {messages[step - 1]}
+            </p>
+            <div className='buttons'>
+                <button style={{ backgroundColor: '#7950F2', color: '#FFFFFF' }} onClick={handlePrevious}>
+                    Previous
+                </button>
+                <button style={{ backgroundColor: '#7950F2', color: '#FFFFFF' }} onClick={handleNext}>
+                    Next
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
